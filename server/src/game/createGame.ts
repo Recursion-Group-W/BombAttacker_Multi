ed8c27824537a7ea2ac5695cdb057aa7cc9cb029
khkmgch <kohei.kamiguchi@gmail.com>
2023-01-08 21:21:46 +0900

    createGame

--- server/src/game/createGame.ts
+++ server/src/game/createGame.ts
@@ -1,21 +1,6 @@
-import '@geckos.io/phaser-on-nodejs'
 import RoomManager from '../manager/roomManager';
-import { config } from './serverConfig';
-import MultiScene from './scene/MultiScene';
-
-type CreateGame = (roomManager: RoomManager, roomId: string, options: { scene: string }) => Phaser.Game
-
-export const createGame:CreateGame = (roomManager: RoomManager, roomId: string, options: { scene: string }) => {
-  if (options.scene === 'MultiScene') {
-    config.scene = [MultiScene]
-  }
-  if(options.scene === 'SingleScene'){
-    // config.scene = [SingleScene]
-  }
-
-const game = new Phaser.Game(config)
-game.registry.set('gameSetting', { roomManager, roomId })
 
 
-  return game
+export const createGame = (roomManager: RoomManager, roomId: string) => {
+ 
 }
