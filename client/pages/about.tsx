6d658f9b023bf8d34693b063c34455b02e19b929
khkmgch <kohei.kamiguchi@gmail.com>
2023-01-08 21:40:37 +0900

    material UIのインストールと設定

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Link from '../src/Link';
 import ProTip from '../src/ProTip';
 import Copyright from '../src/Copyright';
 
-export default function Home() {
+export default function About() {
   return (
     <Container maxWidth="lg">
       <Box
@@ -21,9 +22,11 @@ export default function Home() {
         <Typography variant="h4" component="h1" gutterBottom>
           MUI v5 + Next.js with TypeScript example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
+        <Box maxWidth="sm">
+          <Button variant="contained" component={Link} noLinkStyle href="/">
+            Go to the home page
+          </Button>
+        </Box>
         <ProTip />
         <Copyright />
       </Box>
