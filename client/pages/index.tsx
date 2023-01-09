6cdd79d8c8e8ed56f235265ef79a90b2060c9224
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2023-01-09 17:09:55 +0900

    feat: add login button

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,8 +6,23 @@ import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 import Button from '@mui/material/Button';
 import GoogleIcon from '@mui/icons-material/Google';
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../firebase';
 
 export default function Home() {
+  const [isAuth, setIsAuth] = React.useState(false);
+  const login = () => {
+    signInWithPopup(auth, provider)
+      .then((res) => {
+        console.log(res.user);
+        console.log(res.user.uid);
+        localStorage.setItem('isAuth', 'true');
+        setIsAuth(true);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   return (
     <Container maxWidth='lg'>
       <Box
@@ -27,14 +42,10 @@ export default function Home() {
           BombAttacker_Multi
         </Typography>
 
-        <Box maxWidth='sm'>
-          <Button
-            variant='contained'
-            component={Link}
-            noLinkStyle
-            href={`/lobby/${1}`}
-          >
-            <GoogleIcon />Login/Signin
+        <Box maxWidth='sm' sx={{ py: 2 }}>
+          <Button onClick={login} variant='outlined'>
+            <GoogleIcon />
+            Login/Signin
           </Button>
         </Box>
         <Box maxWidth='sm'>
