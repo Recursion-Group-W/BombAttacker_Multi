f290574375a255265451e60edde514a1855e3e2b
khkmgch <kohei.kamiguchi@gmail.com>
2023-01-21 01:59:53 +0900

    MainScene.ts : サーバから爆弾を受け取る記述。爆弾・プレイヤーを削除する記述。

--- client/src/game/scene/MainScene.ts
+++ client/src/game/scene/MainScene.ts
@@ -1,3 +1,4 @@
+import { BombDto } from '../dto/bomb.dto';
 import { NpcDto } from '../dto/npc.dto';
 import { ObstacleDto } from '../dto/obstacle.dto';
 import { PlayerDto } from '../dto/player.dto';
@@ -7,15 +8,6 @@ import { CustomScene } from './parent/customScene';
 
 export class MainScene extends CustomScene {
   cursor: Cursor | null = null;
-  
-  // public socket: any;
-
-  // objects: { [key: string]: { [id: string]: any } } = {
-  //   playerMap: {},
-  //   npcMap: {},
-  //   obstacleMap: {},
-  //   bombMap: {}
-  // };
 
   constructor() {
     super({ key: 'MainScene' });
@@ -39,39 +31,28 @@ export class MainScene extends CustomScene {
         playerArr: PlayerDto[];
         npcArr: NpcDto[];
         obstacleArr: ObstacleDto[];
-        // bombの配列を追加
-        bombArr: any[]
+        bombArr: BombDto[];
       }) => {
         SyncUtil.setPlayer(res.playerArr, this);
         SyncUtil.setNpc(res.npcArr, this);
         SyncUtil.setObstacle(res.obstacleArr, this);
-        // 1/18
-        // if (res.bombArr.length > 0) {
-        //   res.bombArr.map((obstacle) => {
-        //     if (!this.objects.bombMap[obstacle.id]) {
-        //       let sprite = this.add
-        //         .sprite(
-        //           obstacle.x,
-        //           obstacle.y,
-        //           obstacle.spriteKey
-        //         )
-        //         .setOrigin(0.5);
-        //       this.objects.bombMap[obstacle.id] = {
-        //         sprite: sprite,
-        //       };
-        //     }
-        //     this.objects.bombMap[obstacle.id]['sync'] =
-        //       obstacle;
-        //   });
-        // }
-
+        SyncUtil.setBomb(res.bombArr, this);
       }
     );
+
+    this.socket.on('destroyBomb', (res: { id: number }) => {
+      SyncUtil.destroyBomb(res.id, this);
+    });
+
+    this.socket.on('destroyPlayer', (res: { clientId: string }) => {
+      SyncUtil.destroyPlayer(res.clientId, this);
+    });
   }
   update(): void {
     //syncのデータを基に、spriteの座標とアニメーションを更新
     SyncUtil.updatePlayer(this);
     SyncUtil.updateNpc(this);
+    SyncUtil.updateBomb(this);
 
     this.cursor?.update();
   }
