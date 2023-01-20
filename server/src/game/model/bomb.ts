54c0777c1f059e6d04765933301994aaac976e56
Hayato Koshi <kshyt2501@icloud.com>
2023-01-20 14:25:50 +0900

    adding bomb and explode on server

--- server/src/game/model/bomb.ts
+++ server/src/game/model/bomb.ts
@@ -5,11 +5,27 @@ import { Player } from './player/player';
 export class Bomb extends GameObject {
   static WIDTH = 38.4;
   static HEIGHT = 38.4;
+  private bombExisted: boolean;
   private remainTime: number = 5;
-
+  // private bombs: Phaser.GameObjects.Group;
+  // private bombLog: {
+  //   x: number;
+  //   y: number;
+  //   existed: boolean;
+  // };
+  // private obj = GameObject;
+  objects: { [key: string]: { [id: string]: any } } = {
+    playerMap: {},
+    npcMap: {},
+    obstacleMap: {},
+    bombMap: {},
+    explosionMap: {} 
+  };
   constructor(x: number, y: number, private player: Player) {
     // 親クラスのコンストラクタ呼び出し
     super(x, y, Bomb.WIDTH, Bomb.HEIGHT, 'bomb');
+    // 1/20
+    this.bombExisted = false
   }
 
   update(deltaTime: number) {
@@ -18,9 +34,43 @@ export class Bomb extends GameObject {
       //爆発
       // new Explosion();
     }
+
+    let bomb: any;
+    if (this.player.putBomb()) {
+      if (
+        this.getPosition.x != this.player.getPosition.x ||
+        this.getPosition.y != this.player.getPosition.y ||
+        !this.bombExisted
+      ) {
+        // bomb Map呼び出し方がわからない
+        bomb = this.objects.bombMap.create(
+          this.player.getPosition.x,
+          this.player.getPosition.y,
+          'bomb'
+        );
+        this.bombExisted = true;
+        // this.player.decreaseBombCounter();
+        bomb.anims.play('bomb-anim', true);
+
+        setTimeout(() => {
+          // bomb.xは何のこと？
+          this.setPosition(bomb.x, bomb.y);
+          // this.setPosition = bomb.y;
+          bomb.destroy();
+          this.bombExisted= false;
+          // this.player.increaseBombCounter();
+          setTimeout(() => {
+            this.explosion.clear(true, true);
+            this.player.setHit = false;
+          }, 600);
+        }, 2000);
+
+        }
+    }
+
   }
   // アニメーションを追加
-  // protected animTurnRight() {
-  //   this.animation = `${this.spriteKey}-anim`;
-  // }
+  protected animBomb() {
+    this.animation = `${this.spriteKey}-anim`;
+  }
 }
--- server/src/game/model/explosion.ts
+++ server/src/game/model/explosion.ts
@@ -1,10 +1,63 @@
 import { GameObject } from './gameObject/gameObject';
-
+import { Player } from './player/player';
+import { Bomb } from './bomb';
 export class Explosion extends GameObject {
   static WIDTH = 38.4;
   static HEIGHT = 38.4;
-  //   constructor() {
-  //     super();
-  //   }
-    
+  objects: { [key: string]: { [id: string]: any } } = {
+    playerMap: {},
+    npcMap: {},
+    obstacleMap: {},
+    bombMap: {},
+    explosionMap: {} 
+  };
+  // private explosion = this.objects.explosionMap.create(
+  //   this.player.getPosition.x,
+  //   this.player.getPosition.y,
+  //   'explode'
+  // );
+  
+  // bombの座標は必要なはず？
+  constructor(x: number, y: number, private bomb: Bomb, private player: Player) {
+    super(x, y, Explosion.WIDTH, Explosion.HEIGHT, 'explosion');
+  }
+  
+  updata(){
+    let explosion: any;
+    explosion = this.objects.explosionMap.create(
+      this.player.getPosition.x,
+      this.player.getPosition.y,
+      'explosion'
+    );
+    explosion.create(
+      this.bomb.getPosition.x + 32,
+      this.bomb.getPosition.y,
+      'explosion'
+    );
+    explosion.create(
+      this.bomb.getPosition.x,
+      this.bomb.getPosition.y + 32,
+      'explosion'
+    );
+    explosion.create(
+      this.bomb.getPosition.x - 32,
+      this.bomb.getPosition.y,
+      'explosion'
+    );
+    explosion.create(
+      this.bomb.getPosition.x,
+      this.bomb.getPosition.y - 32,
+      'explosion'
+    );
+    explosion.playAnimation('explosion-anim');
+  }
+  // this.explosion = this.objects.explosionMap.create(
+  //   this.player.getPosition.x,
+  //   this.player.getPosition.y,
+  //   'explode'
+  // );
+  // アニメーションを追加
+  protected animExplosion() {
+    this.animation = `${this.spriteKey}-anim`;
+  }
 }
