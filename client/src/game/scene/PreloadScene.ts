e6e4870190cde2e5a3604770fa334a75de184b05
khkmgch <kohei.kamiguchi@gmail.com>
2023-01-21 01:57:35 +0900

    PreloadScene.ts : 爆弾のアニメーションをSceneに登録

--- client/src/game/scene/PreloadScene.ts
+++ client/src/game/scene/PreloadScene.ts
@@ -9,49 +9,32 @@ export class PreloadScene extends Scene {
   preload() {
     //画像の読み込み
     this.load.image('blueBrick', '/assets/blue_brick.png');
-    this.load.image(
-      'orangeBrick',
-      '/assets/orange_brick.png'
-    );
+    this.load.image('orangeBrick', '/assets/orange_brick.png');
     this.load.image('grayBrick', '/assets/gray_brick.png');
-    this.load.image(
-      'greenBrick',
-      '/assets/green_brick.png'
-    );
+    this.load.image('greenBrick', '/assets/green_brick.png');
 
-    this.load.spritesheet(
-      'player',
-      '/assets/player_spritesheet_fixed.png',
-      {
-        frameWidth: 32,
-        frameHeight: 32,
-      }
-    );
-    this.load.spritesheet(
-      'npc',
-      '/assets/enemy_spritesheet_fixed.png',
-      {
-        frameWidth: 32,
-        frameHeight: 32,
-      }
-    );
+    this.load.spritesheet('player', '/assets/player_spritesheet_fixed.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+    this.load.spritesheet('npc', '/assets/enemy_spritesheet_fixed.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
     this.load.spritesheet('bomb', '/assets/bomb.png', {
       frameWidth: 32,
       frameHeight: 32,
     });
-    this.load.spritesheet(
-      'explosion',
-      '/assets/explode.png',
-      {
-        frameWidth: 32,
-        frameHeight: 32,
-      }
-    );
+    this.load.spritesheet('explosion', '/assets/explode.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
   }
   create() {
     //アニメーションを作成
     AnimationUtil.createPlayerAnim(this);
     AnimationUtil.createNpcAnim(this);
+    AnimationUtil.createBombAnim(this);
   }
 
   update() {
