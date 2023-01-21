40f887ece88ad1b9b77e532dc7f7bfce2bacae50
khkmgch <kohei.kamiguchi@gmail.com>
2023-01-21 18:46:43 +0900

    room/[id].tsx : ブラウザの戻るボタンを無効にする

--- client/pages/room/[id].tsx
+++ client/pages/room/[id].tsx
@@ -1,18 +1,32 @@
 import { Box, Container } from '@mui/material';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 // import GameDisplay from '../../component/GameDisplay';
-const GameDisplay = dynamic(
-  () => import('../../component/GameDisplay'),
-  { ssr: false }
-);
+const GameDisplay = dynamic(() => import('../../component/GameDisplay'), {
+  ssr: false,
+});
 const Room = () => {
   const router = useRouter();
+
+  const browserBack = useCallback(() => {
+    window.history.go(1);
+  }, []);
+
+  const browserReload = useCallback(() => {
+    router.push('/');
+  }, []);
 
   useEffect(() => {
     const { id } = router.query;
     console.log('roomID: ', id);
+    window.addEventListener('popstate', browserBack);
+    window.addEventListener('beforeunload', browserReload);
+
+    return () => {
+      window.removeEventListener('popstate', browserBack);
+      window.removeEventListener('beforeunload', browserReload);
+    };
   }, [router.query]);
   return (
     <>
