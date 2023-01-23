c08568633960e67d85f1ea6a3d913cc7ed812924
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2023-01-23 11:04:15 +0900

    feat: deploy

--- client/env.ts
+++ client/env.ts
@@ -1,3 +1,3 @@
 // .envで管理するべきですが、チーム開発での.envファイルの扱い方が分からないので
 // env.tsを作りました。
-export const NODE_URL = 'http://localhost:5002';
+export const NODE_URL = 'https://backend-bombattacker.an.r.appspot.com';
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import { createServer } from 'http';
+import path from 'path';
 import { Server } from 'socket.io';
 import RoomManager from './manager/roomManager';
 import IoGame from './socket/ioGame';
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.get('*', (req, res) => {
+  res.redirect('https://frontend-bombattacker.an.r.appspot.com/');
+});
 
 const httpServer = createServer(app);
 
@@ -14,14 +18,17 @@ const PORT = 5002;
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://frontend-bombattacker.an.r.appspot.com',
+    ],
   },
 });
 
 //ゲームに関するソケット通信を行うためのNameSpace
 const ioNspGame = io.of('/game');
 //Roomを管理するためのroomManager
-const roomManager = new RoomManager(ioNspGame)
+const roomManager = new RoomManager(ioNspGame);
 const ioGame = new IoGame(ioNspGame, roomManager);
 
 httpServer.listen(process.env.PORT || PORT, () => {
