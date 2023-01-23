5acfb3c3df4fffaa19f4663fd5d8dfa8fc9e283c
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2023-01-23 11:14:15 +0900

    feat: add env for dev

--- client/env.ts
+++ client/env.ts
@@ -1,3 +1,10 @@
 // .envで管理するべきですが、チーム開発での.envファイルの扱い方が分からないので
 // env.tsを作りました。
-export const NODE_URL = 'https://backend-bombattacker.an.r.appspot.com';
+
+// 【ローカル設定】
+// devで起動する場合に使用
+// 本番時にはコメントアウトする
+export const NODE_URL = 'http://localhost:5002';
+// 【本番設定】
+// 開発環境での起動時にはコメントアウトする
+// export const NODE_URL = 'https://backend-bombattacker.an.r.appspot.com';
