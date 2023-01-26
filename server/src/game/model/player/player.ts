64c078aa62d95de0a8a90ad4014eecae36a29856
khkmgch <kohei.kamiguchi@gmail.com>
2023-01-26 20:39:53 +0900

    スコア送信

--- server/src/game/model/player/player.ts
+++ server/src/game/model/player/player.ts
@@ -179,6 +179,14 @@ export class Player extends Character {
 
         //攻撃したプレイヤーのスコアを更新
         explosion.data.player.attackPlayer();
+        roomManager.ioNspGame
+          .to(explosion.data.player.socket.id)
+          .emit('attack', {
+            score: {
+              attackPlayer: explosion.data.player.attackPlayerCount,
+              attackNpc: explosion.data.player.attackNpcCount,
+            },
+          });
 
         //干渉した爆風を削除
         explosionList.remove(explosion);
